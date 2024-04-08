3ff1f2c1aaddf07eec3c62b5cf49ab37ed3f2279
StoermerF <StoermerF@NB21680>
2024-04-08 13:36:32 +0200

    test

--- Test_Montag_VS/Program.vb
+++ Test_Montag_VS/Program.vb
@@ -1,7 +1,13 @@
 Imports System
+Imports System.Windows.Forms
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        'Console.WriteLine("Hello World!")
+        For i = 0 To 5 Step 1
+            Console.WriteLine(i.ToString)
+        Next
+        Console.ReadLine()
+
     End Sub
 End Module
